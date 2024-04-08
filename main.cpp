3af18c18f7bba1b86a0513b8aff72a44d96e3aba
Titouan <titouan@gaelane.org>
2024-04-08 08:55:39 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+
+//---Constantes---//
+const int w = 512;
+const int h = 512;
+
+//---Prototypes---//
+
+//---Main---//
+int main()
+{
+    openWindow(w,h);
+
+
+    endGraphics();
+    return 0;
+}
+
+//---Fonctions---//
